1faa56d0b89cf3ba248f88cfa922d8f2853b2387
nikoloztuzuridze1 <nikoloztuzuridze@gmail.com>
2024-06-26 18:05:01 +0400

    first commit

---
+++ day09/homework/hiiiiiiii.py
@@ -0,0 +1,20 @@
+person1 =input("enter the firt person text")
+
+
+person2 = input("enter the second text")
+
+
+
+print("first person:",person1)
+print("second person:",person2)
+
+
+
+
+
+
+
+
+
+
+
