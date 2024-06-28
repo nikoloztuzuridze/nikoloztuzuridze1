238f9eaf2286a8288405cd896016e199a3472ebc
nikoloztuzuridze1 <nikoloztuzuridzegmail.com>
2024-06-28 15:15:13 +0400

    first commit

---
+++ day13/homework/lesson12.3.py
@@ -0,0 +1,12 @@
+name='nikoloz'
+surname="tuzuridze"
+age=12
+city='tbilisi'
+food="mcvadi"
+favorite_color='black' 
+car='lexus'
+
+
+
+
+print("hi my name",name,surname,"I am",age,"years old","I live in ",city,"my favourite food is",food,"my favorite color is ",favorite_color,"my favorite car is",car)
