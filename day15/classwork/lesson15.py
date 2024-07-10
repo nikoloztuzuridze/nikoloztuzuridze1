f90caacc4e6cb7a171e0dd5ffa19949cbaabc185
nikoloztuzuridze1 <nikoloztuzuridzegmail.com>
2024-07-11 00:09:21 +0400

    first commit

---
+++ day15/classwork/lesson15.py
@@ -0,0 +1,106 @@
+
+
+steps = int(input('enter the stems:'))
+ 
+active_minutes = int(input('enter the minutes:'))
+
+if steps>10000 or active_minutes>30:
+    print(True)
+else:
+    print(False)
+
+
+
+steps = int(input('enter the stems:'))
+ 
+active_minutes = int(input('enter the minutes:'))
+
+if steps>1850 or active_minutes>45:
+    print(True)
+else:
+    print(False)
+
+
+
+
+steps = int(input('enter the stems:'))
+ 
+active_minutes = int(input('enter the minutes:'))
+
+if steps>1850 or active_minutes>25:
+    print(True)
+else:
+    print(False)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ day15/homework/lesson15.py
@@ -0,0 +1,4 @@
+steps = int(input('enter your steps '))
+time = int(input('enter the time'))
+ 
+print(steps//100==time)
